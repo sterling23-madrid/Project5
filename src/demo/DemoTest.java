fee93d638932d2177a9e4c8d8588c98996e887f8
sterling23-madrid <120076006+sterling23-madrid@users.noreply.github.com>
2023-07-27 21:26:43 -0600

    demo class

---
+++ src/demo/DemoTest.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class DemoTest {
+	public static void main(String[] args) {
+		System.out.println("Java is fun");
+		
+		System.out.println("Sterling");
+		
+		System.out.println("madrid ");
+	}
+
+}
